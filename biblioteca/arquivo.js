2aca05ffef4e6f21c17ab25ff46456535989e68b
JoHoSantos <johosenacsantos@gmail.com>
2023-09-13 18:48:46 -0300

    aula 12.09

--- biblioteca/arquivo.js
+++ biblioteca/arquivo.js
@@ -1,17 +1,21 @@
-import chalk from 'chalk';
-import fs from 'fs'
+import chalk from "chalk";
 
-function tratarError(erro){
-    throw new Error(chalk.red(erro.code,'não há arquivo no caminho'));
+import fs from 'fs';
+
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code,'não há arquivos no caminho' ))
 }
 
 function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
+    const encoding ='utf-8';
+    fs.readFile(caminhoDoArquivo, encoding,(erro,texto) => {
         if (erro){
-            tratarError(erro);
+            trataErro(erro);
         }
         console.log(chalk.blue(texto));
+    
     })
 }
-pegaArquivo('text.md')
+
+pegaArquivo('texto.md')
